c13b31ff72896eb488aa39ef13705586886ebad5
F-zeinali <fatemeh.zeinali7026@gmail.com>
2024-04-21 09:23:35 +0330

    remove clicked product

--- script.js
+++ script.js
@@ -59,7 +59,9 @@ let shoppingCart = [];
 let $ = document;
 let productCardContainer = $.querySelector(".product-card-container");
 let shopCartContainer = $.querySelector(".basket-item");
+let totalPriceElem = $.querySelector(".total-price");
 
+productCardContainer.innerHTML = "";
 allProducts.forEach(function (product) {
   productCardContainer.insertAdjacentHTML(
     "beforeend",
@@ -82,24 +84,65 @@ function addProductToCart(productId) {
     return product.id === productId;
   });
 
-  shoppingCart.push(selectedProduct);
-  cartProductGenerator(shoppingCart);
-  console.log(shoppingCart);
+  let hasProduct = shoppingCart.some(function (product) {
+    return product.id === selectedProduct.id;
+  });
+  if (hasProduct) {
+    alert("product has been already added");
+  } else {
+    shoppingCart.push(selectedProduct);
+    cartProductGenerator(shoppingCart);
+  }
 }
 
 function cartProductGenerator(allProductsArray) {
   shopCartContainer.innerHTML = "";
+
   allProductsArray.forEach(function (product) {
-    shopCartContainer.insertAdjacentHTML(
-      "beforeend",
-      '<ol class="list-group shop-cart p-2"><li class="list-group-item shop-cart-item d-flex justify-content-between align-items-center pb-3">' +
-        product.title +
-        '<span class="product-price">' +
-        product.price +
-        '$</span><span class="product-count">' +
-        product.count +
-        '</span><i class="fa fa-trash-o delete" ></i><i class="fa fa-plus plus"></i></li></ol>'
+    let cartProductList = $.createElement("ol");
+    cartProductList.className = "list-group shop-cart p-2";
+
+    let cartProduct = $.createElement("li");
+    cartProduct.className =
+      "list-group-item shop-cart-item d-flex justify-content-between align-items-center pb-3";
+    cartProduct.innerHTML = product.title;
+
+    let cartProductPrice = $.createElement("span");
+    cartProductPrice.className = "product-price";
+    cartProductPrice.innerHTML = product.price;
+
+    let cartProductCount = $.createElement("span");
+    cartProductCount.className = "product-count";
+    cartProductCount.innerHTML = product.count;
+
+    let cartProductDelete = $.createElement("i");
+    cartProductDelete.className = "fa fa-trash-o delete";
+    cartProductDelete.addEventListener("click", function () {
+      removeProduct(product.id);
+    });
+
+    let cartProducrAdd = $.createElement("i");
+    cartProducrAdd.className = "fa fa-plus plus inc";
+    cartProducrAdd.addEventListener("click", function () {});
+
+    cartProduct.append(
+      cartProductPrice,
+      cartProductCount,
+      cartProductDelete,
+      cartProducrAdd
     );
+    cartProductList.append(cartProduct);
+    shopCartContainer.appendChild(cartProductList);
   });
 }
 
+//remove selected product
+function removeProduct(productId) {
+  //update value of shopping cart
+  shoppingCart = shoppingCart.filter(function (product) {
+    //return products that their id are not the same as clicked product
+    return product.id !== productId;
+  });
+  cartProductGenerator(shoppingCart);
+  console.log(shoppingCart);
+}
