78bfcabffcd7e72b06c92798050f122618df46ed
F-zeinali <fatemeh.zeinali7026@gmail.com>
2024-06-16 23:50:40 +0330

    generate swiper ui

--- home-page.js
+++ home-page.js
@@ -48,4 +48,46 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
   generateCarousleItem();
+
+  //Swiper Slider(Category)
+  let categorySwiperList = [
+    { id: 1, title: "Children's Books", src: "assets/images/children-cat.svg" },
+    { id: 2, title: "Young Adult", src: "assets/images/young-adult-cat.svg" },
+    {
+      id: 3,
+      title: "Autobiographies",
+      src: "assets/images/autobiographic-cat.svg",
+    },
+    { id: 4, title: "Business", src: "assets/images/business-cat.svg" },
+    { id: 5, title: "Fiction", src: "assets/images/fiction-cat.svg" },
+    { id: 6, title: "Comics", src: "assets/images/comics-cat.svg" },
+    { id: 7, title: "Self-help", src: "assets/images/selfhelp-cat.svg" },
+    { id: 8, title: "Cooking", src: "assets/images/cooking-cat.svg" },
+  ];
+
+  function categoriesGeneration() {
+    let swiperSliderElem = document.querySelector(".swiper-slider");
+
+    categorySwiperList.forEach((category) => {
+      //swiper item
+      let swiperItem = document.createElement("div");
+      swiperItem.classList.add("swiper-item");
+      //swiper container
+      let swiperContainerElem = document.createElement("div");
+      swiperContainerElem.classList.add("swiper-container");
+
+      let swiperImg = document.createElement("img");
+      swiperImg.classList.add("swiper-icon");
+      swiperImg.setAttribute("src", category.src);
+
+      let swiperTitle = document.createElement("p");
+      swiperTitle.classList.add("swiper-title");
+      swiperTitle.innerHTML = category.title;
+
+      swiperContainerElem.append(swiperImg, swiperTitle);
+      swiperItem.append(swiperContainerElem);
+      swiperSliderElem.append(swiperItem);
+    });
+  }
+  categoriesGeneration();
 });
